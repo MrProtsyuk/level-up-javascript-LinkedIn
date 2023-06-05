bcf141be932ff87d4b7620c5ccd304b426270cfc
MrProtsyuk <94588855+MrProtsyuk@users.noreply.github.com>
2023-06-05 18:44:27 +0000

    completed coffee calculations challenge

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,9 @@
 // Write your code here
+function coffeeDate(coffeeArray) {
+  var coffeeSum = coffeeArray.reduce(
+    (totalCoffees, numCoffees) => (totalCoffees += numCoffees)
+  );
+  return `The total coffee bill is $${coffeeSum * 1.25}`;
+}
+
+console.log(coffeeDate([2, 5, 7, 1, 4]));
