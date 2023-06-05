0669d33edaaeb6ff3f85c0e2062066f511f89698
MrProtsyuk <94588855+MrProtsyuk@users.noreply.github.com>
2023-06-05 18:34:15 +0000

    completed classroom attendence challenge

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,17 @@
 // Write your code here
+function getStudents(classroom) {
+  var { hasTeachingAssistant, classList } = classroom;
+  var teacher, teachingAssistant, students;
+
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: true,
+  classList: ["Tom", "John", "Jane", "Kane"]
+}));
