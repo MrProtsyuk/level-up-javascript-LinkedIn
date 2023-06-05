d3510d07909ac7d4ae924d9255c7a885f06845c5
MrProtsyuk <94588855+MrProtsyuk@users.noreply.github.com>
2023-06-05 18:21:12 +0000

    did private objects challenge

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "Mark", 
+  [password]: "Mark",
+  age: 16
+};
+
+console.log(user.username);
+console.log(user.password);
