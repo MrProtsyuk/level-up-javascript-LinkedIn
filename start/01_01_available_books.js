b159023b28214bc1040cac55e832418cbfc4caa4
MrProtsyuk <94588855+MrProtsyuk@users.noreply.github.com>
2023-06-05 18:12:28 +0000

    worked on the first three coding challenges

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,30 @@
 // Write your code here
+
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  get availability() {
+    return this.getAvailability();
+  }
+
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "This book is out of stock";
+    } else if (this.numCopies < 10) {
+      return "Low stock";
+    }
+    return "In Stock";
+  }
+    sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+  
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+}
--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,17 @@
 // Write your code here
+function Movie(title, director, genre, releaseYear, rating) {
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.releaseYear = releaseYear;
+  this.rating = rating;
+}
+
+Movie.prototype.getOverview = function() {
+  return `${this.title}, a ${this.genre} file directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`
+}
+
+const Marky = new Movie ("Marky", "MaRky", "Scary", 2015, "5/5");
+
+console.log(Marky);
+console.log(Marky.getOverview());
